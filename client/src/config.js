605df127423546d4a5957e8fd0bdb93b4a413c18
shinefiling <shinefiling@gmail.com>
2026-02-19 13:07:21 +0530

    Fix mixed content error

--- client/src/config.js
+++ client/src/config.js
@@ -1,7 +1,6 @@
 const isProduction = window.location.hostname === 'uliyar.com' || window.location.hostname === 'www.uliyar.com';
 export const API_BASE_URL = isProduction
-    ? 'http://uliyar.com'
-
+    ? `${window.location.protocol}//${window.location.hostname}`
     : `http://${window.location.hostname}:8082`;
 
 // If the backend is served on the same domain but different port during dev
